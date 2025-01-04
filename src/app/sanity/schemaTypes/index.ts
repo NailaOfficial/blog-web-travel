fec15397942f4de8a8895c7d9b944ed8ded83e86
NailaOfficial <nameerofficial@gmail.com>
2025-01-04 20:23:04 +0500

    Delete src/app/sanity directory

--- src/app/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { Category } from './category'
-import { Feature } from './feature_cnt'
-import {BlogData} from '../schemaTypes/blogsanity'
- export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [Category,Feature , BlogData ],
-}
